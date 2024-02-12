e9fac49d7f88b2204ad58a08032a373ef0cbcc53
Ereyna21 <153467127+Ereyna21@users.noreply.github.com>
2024-02-12 12:53:10 -0600

    created user seed

--- seeds/fakerSeedUser.js
+++ seeds/fakerSeedUser.js
@@ -1,1 +1,13 @@
-//for users
+const faker = require('faker');
+
+function createRandomUser() {
+  return {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
+
+const generateFakeUsers = (count) => faker.helpers.multiple(createFakeUser, { count });
+
+module.exports = USERS;
